546a0cd2e03fe7f5aea8cbcef43dc7321ea35aa9
vinicius-guima <vinicius_guima.social@hotmail.com>
2019-01-16 00:21:43 -0200

    Salvando sessao no sistema

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,6 +21,9 @@ public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
+    
+    @Autowired
+    private SessaoDao sessaoDao;
 
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
@@ -34,9 +38,6 @@ public class SalaController {
 
         return modelAndView;
     }
-
-
-
 
     @PostMapping("/admin/sala")
     @Transactional
@@ -66,6 +67,7 @@ public class SalaController {
         Sala sala = salaDao.findOne(id);
 
         ModelAndView view = new ModelAndView("sessao/lista");
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
         view.addObject("sala", sala);
 
         return view;
@@ -87,6 +89,6 @@ public class SalaController {
     @ResponseBody
     @Transactional
     public void delete(@PathVariable("id") Integer id){
-        salaDao.delete(id);
-    }
+		salaDao.delete(id);
+	}
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,21 +2,38 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
 	
 		public Sessao(LocalTime horario , Sala sala, Filme filme) {
-
 			this.horario = horario;
 			this.sala = sala;
 			this.filme = filme;
 		}
-	
+		
+       /*		
+       * @deprecated hibernate only
+       */		
+		public Sessao() {
+		}
 		
 		public LocalTime getHoraioTermino() {
 			return this.horario.plusMinutes(filme.getDuracao().toMinutes());
